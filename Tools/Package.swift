507777ced48fc4de21cab51f035d87c145bcb632
binishmaharjan <legendbinish124@gmail.com>
2020-06-19 01:04:52 +0900

    add tools

---
+++ Tools/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "Tool",
+    platforms: [.macOS(.v10_14)],
+    dependencies: [
+        .package(url: "https://github.com/yonaskolb/XcodeGen.git", .exact("2.15.1")),
+    ],
+    targets: [
+        .target(
+            name: "Dummy",
+            dependencies: [
+                .product(name: "xcodegen", package: "XcodeGen"),
+            ],
+            path: ""
+        ),
+    ]
+)
